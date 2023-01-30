3dbca19c102f6df3a03a82aec3ab7c38d321334e
Oleg Kuibar <oleg.kuibar@proton.me>
2023-01-30 00:24:50 -0500

    feat: add dashboard page

---
+++ ui/app/(code-coverage-monitor)/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+// Nextjs Layout Component
+
+import React from 'react';
+
+const Layout = ({children}: { children: React.ReactNode }) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default Layout;
---
+++ ui/app/(code-coverage-monitor)/dashboard/page.tsx
@@ -0,0 +1,16 @@
+// Dashboard page component
+// This component is the main page of the dashboard
+//
+// Path: ui/app/(code-coverage-monitor)/dashboard/page.tsx
+
+import React from "react";
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    );
+};
+
+export default Page;
